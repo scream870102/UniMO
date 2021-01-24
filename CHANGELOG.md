697c8cf4e5298d653d204237c33270f25bc286ce
Scream <scream870102@gmail.com>
2021-01-24 14:56:52 +0900

    Update to 1.1.0 Add JsonFileUtility

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,5 +1,8 @@
-# 1.0.1
-- Disable Scream.UniMO.Input.GamepadControler
+# 1.1.0
+- Add JsonFileUtility
 
 # 1.0.2
 - Fix Scream.UniMO.Editor Build Error
+
+# 1.0.1
+- Disable Scream.UniMO.Input.GamepadControler
---
+++ Runtime/Utils/JsonFileUtility.cs
@@ -0,0 +1,36 @@
+﻿using System.IO;
+using UnityEngine;
+
+namespace Scream.UniMO.Utils
+{
+    public class JsonFileUtility
+    {
+        static public void CreateJsonFile<T>(T obj, string path, string fileName)
+        {
+            CheckDirectory(path);
+            var fullPath = Path.Combine(path, fileName);
+            if (File.Exists(fullPath))
+            {
+                Debug.Log($"{fullPath} already exists");
+                return;
+            }
+            var fs = new FileStream(fullPath, FileMode.Create);
+            string fileContext = JsonUtility.ToJson(obj);
+            var file = new StreamWriter(fs);
+            file.Write(fileContext);
+            file.Close();
+        }
+
+        static public T Load<T>(string path)
+        {
+            if (!File.Exists(path)) return default(T);
+            return JsonUtility.FromJson<T>(File.ReadAllText(path));
+        }
+
+        static void CheckDirectory(string path)
+        {
+            if (!Directory.Exists(path))
+                Directory.CreateDirectory(path);
+        }
+    }
+}
