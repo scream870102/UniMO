aa58ee0c64417a587f45a09fdda3fdd3f1e3526c
Scream <scream870102@gmail.com>
2021-09-20 00:33:01 +0800

    Update to 1.5.0 Container Update

--- Runtime/Collections/Container.cs
+++ Runtime/Collections/Container.cs
@@ -1,67 +1,205 @@
 using System;
 using System.Collections.Generic;
-using UnityEngine;
 
 namespace Scream.UniMO.Collections
-{   
+{
     /// <summary>
     /// Use this scriptableobject to store all object you need.
     /// <para>Inherit this class to fit your own object</para>
     /// </summary>
     /// <typeparam name="T">type of key</typeparam>
     /// <typeparam name="U">type of target object</typeparam>
-    public class Container<T, U> : ScriptableObject
-    where T : IConvertible
+    [Serializable]
+    public class Container<T, U>
+    where T : struct
     {
-        /// <summary>
-        /// Store your objects and key here
-        /// </summary>
-        public ObjectContainer<T, U>[] Objects = null;
-
-        /// <summary>
-        /// Convert to list and ignore key
-        /// </summary>
-        /// <returns>convert the array of Objects to list</returns>
-        public virtual List<U> ToList()
-        {
-            var result = new List<U>();
-            foreach (var o in Objects)
-                result.Add(o.Object);
-            return result;
-        }
+        public T Key;
+        public U Value;
 
         /// <summary>
         /// Convert to dictionary
         /// </summary>
+        /// <param name="collection">which collection to be found</param>
         /// <returns>Convert Objects to dictionary</returns>
-        public virtual Dictionary<T, U> ToDictionary()
+        public static Dictionary<T, U> ToDictionary(IEnumerable<Container<T, U>> collection)
         {
             var result = new Dictionary<T, U>();
-            foreach (var o in Objects)
+            foreach (var o in collection)
             {
                 if (result.ContainsKey(o.Key))
                 {
-                    Debug.LogError($"{o.Key} already exists");
                     continue;
                 }
-                result.Add(o.Key, o.Object);
+                result.Add(o.Key, o.Value);
             }
             return result;
+        }
+
+        /// <summary>
+        /// Convert to list and ignore key
+        /// </summary>
+        /// <param name="collection">which collection to be found</param>
+        /// <returns>convert the array of Objects to list</returns>
+        public static List<U> ToList(IEnumerable<Container<T, U>> collection)
+        {
+            var result = new List<U>();
+            foreach (var o in collection)
+                result.Add(o.Value);
+            return result;
+        }
+
+        /// <summary>
+        /// Check if this collection contains specific value
+        /// </summary>
+        /// <param name="value">value to find</param>
+        /// <param name="collection">which collection to be found</param>
+        /// <returns>return the result</returns>
+        public static bool ContainsValue(U value, IEnumerable<Container<T, U>> collection)
+        {
+            foreach (var pair in collection)
+            {
+                if (pair.Value.Equals(value))
+                {
+                    return true;
+                }
+            }
+            return false;
+        }
+
+        /// <summary>
+        /// Check if this collection contains specific key 
+        /// </summary>
+        /// <param name="key">key to find</param>
+        /// <param name="collection">which collection to be found</param>
+        /// <returns>return the result</returns>
+        public static bool ContainsKey(T key, IEnumerable<Container<T, U>> collection)
+        {
+            foreach (var pair in collection)
+            {
+                if (pair.Key.Equals(key))
+                {
+                    return true;
+                }
+            }
+            return false;
+        }
+
+        /// <summary>
+        /// Get all the keys from a collection
+        /// </summary>
+        /// <param name="collection">resource collection</param>
+        /// <returns>all the keys as array</returns>
+        public static T[] Keys(IEnumerable<Container<T, U>> collection)
+        {
+            var result = new List<T>();
+            foreach (var pair in collection)
+            {
+                result.Add(pair.Key);
+            }
+            return result.ToArray();
+        }
+
+        /// <summary>
+        /// Get all the values from a collection
+        /// </summary>
+        /// <param name="collection">resource collection</param>
+        /// <returns>all the values as array</returns>
+        public static U[] Values(IEnumerable<Container<T, U>> collection)
+        {
+            var result = new List<U>();
+            foreach (var pair in collection)
+            {
+                result.Add(pair.Value);
+            }
+            return result.ToArray();
         }
 
     }
 
     /// <summary>
-    /// Container Type
+    /// ContainerExtension contains extension method for container class
     /// </summary>
-    /// <typeparam name="T">type of key</typeparam>
-    /// <typeparam name="U">type of object</typeparam>
-    [Serializable]
-    public class ObjectContainer<T, U>
-    where T : IConvertible
+    public static class ContainerExtension
     {
-        public T Key;
-        public U Object;
+        /// <summary>
+        /// Convert to list and ignore key
+        /// </summary>
+        /// <param name="collection">resource collection</param>
+        /// <typeparam name="T">type of key</typeparam>
+        /// <typeparam name="U">type of value</typeparam>
+        /// <returns>all object to list</returns>
+        public static List<U> ToList<T, U>(this IEnumerable<Container<T, U>> collection)
+        where T : struct
+        {
+            return Container<T, U>.ToList(collection);
+        }
+
+        /// <summary>
+        /// Convert to dictionary ,ignore value key pair if key already exists
+        /// </summary>
+        /// <param name="collection">resource collection</param>
+        /// <typeparam name="T">type of key</typeparam>
+        /// <typeparam name="U">type of value</typeparam>
+        /// <returns>all object to dictionary</returns>
+        public static Dictionary<T, U> ToDictionary<T, U>(this IEnumerable<Container<T, U>> collection)
+        where T : struct
+        {
+            return Container<T, U>.ToDictionary(collection);
+        }
+
+        /// <summary>
+        /// Check if this collection contains specific value
+        /// </summary>
+        /// <param name="collection">resource collection</param>
+        /// <param name="value">value to find</param>
+        /// <typeparam name="T">type of key</typeparam>
+        /// <typeparam name="U">type of value</typeparam>
+        /// <returns>result</returns>
+        public static bool ContainsValue<T, U>(this IEnumerable<Container<T, U>> collection, U value)
+        where T : struct
+        {
+            return Container<T, U>.ContainsValue(value, collection);
+        }
+
+        /// <summary>
+        /// Check if this collection contains specific key 
+        /// </summary>
+        /// <param name="collection">resource collection</param>
+        /// <param name="key">key to find</param>
+        /// <typeparam name="T">type of key</typeparam>
+        /// <typeparam name="U">type of value</typeparam>
+        /// <returns>result</returns>
+        public static bool ContainsKey<T, U>(this IEnumerable<Container<T, U>> collection, T key)
+        where T : struct
+        {
+            return Container<T, U>.ContainsKey(key, collection);
+        }
+
+        /// <summary>
+        /// Get all the keys from a collection
+        /// </summary>
+        /// <param name="collection">resource collection</param>
+        /// <typeparam name="T">type of key</typeparam>
+        /// <typeparam name="U">type of value</typeparam>
+        /// <returns>all the keys as array</returns>
+        public static T[] Keys<T, U>(this IEnumerable<Container<T, U>> collection)
+        where T : struct
+        {
+            return Container<T, U>.Keys(collection);
+        }
+
+        /// <summary>
+        /// Get all the values from a collection
+        /// </summary>
+        /// <param name="collection">resource collection</param>
+        /// <typeparam name="T">type of key</typeparam>
+        /// <typeparam name="U">type of value</typeparam>
+        /// <returns>all the values as array</returns>
+        public static U[] Values<T, U>(this IEnumerable<Container<T, U>> collection)
+        where T : struct
+        {
+            return Container<T, U>.Values(collection);
+        }
+
     }
 }
-
--- Runtime/Collections/PrefabContainer.cs
+++ Runtime/Collections/PrefabContainer.cs
@@ -1,6 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 using Object = UnityEngine.Object;
 
 namespace Scream.UniMO.Collections
@@ -9,51 +7,9 @@ namespace Scream.UniMO.Collections
     /// Use this scriptableobject to store all prefab you need.
     /// </summary>
     [CreateAssetMenu(fileName = "PrefabContainer", menuName = "ScriptableObjects/PrefabContainer")]
-    public class PrefabContainer : Container<string, Object>
+    public class PrefabContainer : ScriptableObject
     {
-
-        /// <summary>
-        /// Convert object to other type
-        /// </summary>
-        /// <typeparam name="T">target type of object</typeparam>
-        /// <returns>Objects list</returns>
-        public List<T> ToList<T>()
-        where T : Object
-        {
-            var result = new List<T>();
-            foreach (var o in Objects)
-                result.Add((T)o.Object);
-            return result;
-        }
-
-
-        /// <summary>
-        /// Convert key from string to basic type
-        /// </summary>
-        /// <typeparam name="T">Target key type</typeparam>
-        /// <typeparam name="U">Object type</typeparam>
-        /// <returns>Convert key to specif type and return as dictionary</returns>
-        public Dictionary<T, U> ToDictionary<T, U>()
-        where T : IConvertible
-        where U : Object
-        {
-            var result = new Dictionary<T, U>();
-            foreach (var o in Objects)
-            {
-                T key;
-                if (typeof(T).IsEnum)
-                    key = (T)Enum.Parse(typeof(T), o.Key);
-                else
-                    key = (T)Convert.ChangeType(o.Key, typeof(T));
-                if (result.ContainsKey(key))
-                {
-                    Debug.LogError($"{o.Key} already exists");
-                    continue;
-                }
-                result.Add(key, (U)o.Object);
-            }
-            return result;
-        }
+        public Container<string, Object>[] PrefabDictionary;
     }
 }
 
